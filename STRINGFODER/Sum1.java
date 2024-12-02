0d53018c2a09274b37834cf69c9011a3663b3d3f
Mary Syokau <160385341+QueenMary100@users.noreply.github.com>
2024-12-02 20:24:30 +0300

    Create Sum1.java

---
+++ STRINGFODER/Sum1.java
@@ -0,0 +1,12 @@
+public class Sum1{
+  public static void main(String args[]){
+    int num1,num2, sum;
+
+    num1 = 10;
+    num2 = 20;
+    sum = num1 + num2;
+
+    System.out.println("The sum is "+sum);
+  
+  }
+}
