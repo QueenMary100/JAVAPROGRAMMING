592034b2e7a48d031222de922d2c1c70c140d839
QueenMary100 <marysyokau741@gmail.com>
2024-12-03 18:48:25 +0300

    modify class load

--- DATABASEGUIFOLDER/src/bankConnection.java
+++ DATABASEGUIFOLDER/src/bankConnection.java
@@ -8,24 +8,23 @@ import javax.swing.JOptionPane;
 public class bankConnection{
 
     //private static  Connection conn = DriverManager.getConnection("jdbc://locahost:3306/bank", "root", "root");
-    public static Connection getConnection(){
+    public static Connection getConnection() throws ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = null;
-        try{
+        try {
             String url = "jdbc:mysql://localhost:3306/bank";
             String user = "root";
             String passwoord = "root";
 
             conn = DriverManager.getConnection(url, user, passwoord);
             System.out.println("Connection Successful");
-        }catch(SQLException e){
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error Connecting to the database");
             e.printStackTrace();
             System.out.println("Error connecting to the database");
         }
 
         return conn;
-
     }
 
     public static void main(String[] args) {
