0f27681b701a9069d9f1c702210e4c16735857ac
QueenMary100 <marysyokau741@gmail.com>
2024-12-09 22:08:37 +0300

    multual connection

--- Sockets/SocketServer.java
+++ Sockets/SocketServer.java
@@ -7,35 +7,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServer {
-public static void main(String[] args) {
-    try (ServerSocket servSocket = new ServerSocket(12345)) {
-        System.out.println("Waiting for connection...");
+    public static void main(String[] args) {
+        try (ServerSocket servSocket = new ServerSocket(12345)) {
+            System.out.println("Waiting for connection...");
 
-        Socket clientSocket = servSocket.accept();
-        System.out.println("Connection established!");
+            Socket clientSocket = servSocket.accept();
+            System.out.println("Connection established!");
 
-        try{
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        } {
-            String clientMessage;
-            while(clientMessage = in.readline() != null){
+            try{
+                BufferedReader inn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                
+                String clientMessage;
+                while((clientMessage = inn.readLine()) != null){
 
-                System.out.println("Client says: " + clientMessage);
-               
-                if((clientMessage.trim().equalsIgnoreCase("goodbye"))){
-                    System.out.println("Goodbye connection closing");
-                    System.out.println("client disconnected");
-                    break;
+                    System.out.println("Client says: " + clientMessage);
+                
+                    if((clientMessage.trim().equalsIgnoreCase("goodbye"))){
+                        System.out.println("Goodbye connection closing");
+                        System.out.println("client disconnected");
+                        break;
+                    }
+                    System.out.println("Server"+clientMessage);
                 }
-                System.out.println("Server"+clientMessage);
-            }
-        }catch(IOException e){
-            System.out.println("Error in communication from client: " + e.getMessage());
+                
+            }catch(IOException e){
+                System.out.println("Error in communication from client: " + e.getMessage());
+                }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(" Server error"+ e.getMessage());
         }
-    } catch (Exception e) {
-        // TODO: handle exception
-        System.out.println(" Server error"+ e.getMessage());
+
     }
-}
 }
